5aab8c7a18253dc3dcc0f8e7ae2b7be4b833979e
Yaraoliver <55423791+yaraoliver@users.noreply.github.com>
2020-06-09 20:39:24 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,14 @@
+var contador="";
+function myFunction(num){
+	valor=parseInt('num');
+	console.log (num, typeof num);
+}
+/*function myresult(){
+	document.getElementsByClassName('visor_calculadora').innerHTML= contador;
+	console.log(contador);
+}
+function reset(){
+	
+	
+*/
+
